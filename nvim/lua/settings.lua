08617bfafb315b1aeb7f19ed4fed33e8c996e4c0
Gustaf Lindstedt <gustaflindstedt@protonmail.com>
2023-08-04 23:51:31 +0200

    configure diagnostic signs

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -36,3 +36,34 @@ vim.opt.foldminlines = 20
 
 -- Use python parser for bzl files
 vim.treesitter.language.register("python", "bzl")
+
+-- Configuration for diagnostics
+local signs = {
+  { name = "DiagnosticSignError", text = "" },
+  { name = "DiagnosticSignWarn", text = "" },
+  { name = "DiagnosticSignHint", text = "" },
+  { name = "DiagnosticSignInfo", text = "" },
+}
+
+for _, sign in ipairs(signs) do
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+end
+
+local config = {
+  signs = {
+    active = signs, -- show signs
+  },
+  update_in_insert = true,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = true,
+    style = "minimal",
+    border = "single",
+    source = "always",
+    header = "Diagnostic",
+    prefix = "",
+  },
+}
+
+vim.diagnostic.config(config)
